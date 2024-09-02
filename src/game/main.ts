133e77108171de91de2cbe6c7a6577cec8d96060
Gegham Ayvazyan <gegham.aivazyan@gmail.com>
2024-09-02 17:47:28 +0400

    change

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,9 +1,9 @@
-import { Boot } from './scenes/Boot';
-import { GameOver } from './scenes/GameOver';
-import { Game as MainGame } from './scenes/Game';
-import { MainMenu } from './scenes/MainMenu';
-import { AUTO, Game } from 'phaser';
-import { Preloader } from './scenes/Preloader';
+import {Boot} from './scenes/Boot';
+import {GameOver} from './scenes/GameOver';
+import {Game as MainGame} from './scenes/Game';
+import {MainMenu} from './scenes/MainMenu';
+import {AUTO, Game} from 'phaser';
+import {Preloader} from './scenes/Preloader';
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
@@ -24,7 +24,7 @@ const config: Phaser.Types.Core.GameConfig = {
 
 const StartGame = (parent: string) => {
 
-    return new Game({ ...config, parent });
+    return new Game({...config, parent});
 
 }
 
