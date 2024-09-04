30ea02c3fc935d9e2769d4b2520272c848a5afeb
Gegham Ayvazyan <gegham.aivazyan@gmail.com>
2024-09-04 12:50:44 +0400

    main menu & start game

--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -4,17 +4,31 @@ import {EventBus} from '../EventBus';
 
 export class MainMenu extends Scene {
     title: GameObjects.Text;
+    startGame: GameObjects.Text;
 
     constructor() {
         super('MainMenu');
     }
 
     create() {
-        this.title = this.add.text(window.innerWidth / 2, window.innerHeight / 2, 'Main Menu', {
+        this.title = this.add.text(window.innerWidth / 2, window.innerHeight / 2 - 100, 'Main Menu', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         }).setOrigin(0.5).setDepth(100);
+        
+        this.startGame = this.add.text(window.innerWidth / 2, window.innerHeight / 2, 'Start Game', {
+            fontFamily: 'Arial Black', fontSize: 32, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5).setDepth(100);
+        
+        this.startGame.setInteractive()
+        
+        this.startGame.on('pointerdown', () => {
+            this.scene.start('MainScene')
+        });
+
 
         EventBus.emit('current-scene-ready', this);
     }
--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -49,7 +49,7 @@ export class Preloader extends Scene {
         this.registerAnimations();
 
         //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
-        this.scene.start('MainScene');
+        this.scene.start('MainMenu');
     }
 
     private registerAnimations() {
