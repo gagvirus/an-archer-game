fa1cc6de2ec04e9ca5d71647609a04b94203e444
Gegham Ayvazyan <gegham.aivazyan@gmail.com>
2024-09-04 13:10:06 +0400

    go to main menu button on pause menu

--- src/game/scenes/PauseMenu.ts
+++ src/game/scenes/PauseMenu.ts
@@ -5,21 +5,29 @@ import {createCenteredText} from "../helpers/text-helpers.ts";
 
 export class PauseMenu extends Scene {
     title: GameObjects.Text;
-    startGame: GameObjects.Text;
+    resume: GameObjects.Text;
+    backToMainMenu: GameObjects.Text;
 
     constructor() {
         super('PauseMenu');
     }
 
     create() {
-        this.title = createCenteredText(this, 'Pause Menu', -100, 38, false);
-        this.startGame = createCenteredText(this, 'Resume', 0, 32, true);
+        this.title = createCenteredText(this, 'Pause Menu', -150, 38, false);
+        this.resume = createCenteredText(this, 'Resume', -75, 32, true);
+        this.backToMainMenu = createCenteredText(this, 'Back to Main Menu', 0, 32, true);
         
-        this.startGame.setInteractive()
+        this.resume.setInteractive()
         
-        this.startGame.on('pointerdown', () => {
+        this.resume.on('pointerdown', () => {
             this.scene.resume('MainScene')
             this.scene.stop();
+        });
+        
+        this.backToMainMenu.on('pointerdown', () => {
+            this.scene.stop('MainScene');
+            this.scene.stop();
+            this.scene.start('MainMenu')
         });
 
 
