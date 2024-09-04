3d9fc37773512fb6488ba58f8c2433808c4c7c51
Gegham Ayvazyan <gegham.aivazyan@gmail.com>
2024-09-04 11:43:17 +0400

    cleanup

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,6 +1,5 @@
 import {Boot} from './scenes/Boot';
 import {GameOver} from './scenes/GameOver';
-import {Game as MainGame} from './scenes/Game';
 import {MainMenu} from './scenes/MainMenu';
 import {AUTO, Game} from 'phaser';
 import {Preloader} from './scenes/Preloader';
@@ -22,7 +21,6 @@ const config: Phaser.Types.Core.GameConfig = {
         Boot,
         Preloader,
         MainMenu,
-        MainGame,
         GameOver,
         MainScene,
     ],
