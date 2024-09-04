f37fd250a3465409c166b82271f3df786145df83
Gegham Ayvazyan <gegham.aivazyan@gmail.com>
2024-09-04 15:28:26 +0400

    spawn away from hero

--- src/game/helpers/position-helper.ts
+++ src/game/helpers/position-helper.ts
@@ -1,8 +1,21 @@
-const getRandomPosition =
-    (maxX: number, maxY: number) => {
-        const x = Phaser.Math.Between(50, maxX - 50);
-        const y = Phaser.Math.Between(50, maxY - 50);
-        return {x, y};
-    }
+interface Vector2 {
+    x: number;
+    y: number;
+}
 
-export {getRandomPosition}
+const getRandomPosition = (maxX: number, maxY: number): Vector2 => {
+    const x = Phaser.Math.Between(50, maxX - 50);
+    const y = Phaser.Math.Between(50, maxY - 50);
+    return {x, y};
+}
+
+const getRandomPositionAwayFromPoint = (maxX: number, maxY: number, awayFrom: Vector2, minDistance: number): Vector2 => {
+    const {x, y} = getRandomPosition(maxX, maxY);
+    const distance = Phaser.Math.Distance.Between(awayFrom.x, awayFrom.y, x, y);
+    if (distance < minDistance) {
+        return getRandomPositionAwayFromPoint(maxX, maxY, awayFrom, minDistance)
+    }
+    return {x, y}
+}
+
+export {getRandomPosition, getRandomPositionAwayFromPoint}
