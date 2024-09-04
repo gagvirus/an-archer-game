4b924ae8e54b2d0039aea035474d53fbb31f9384
Gegham Ayvazyan <gegham.aivazyan@gmail.com>
2024-09-04 12:58:11 +0400

    using helper

--- src/game/scenes/GameOver.ts
+++ src/game/scenes/GameOver.ts
@@ -1,9 +1,11 @@
 import {EventBus} from '../EventBus';
 import {Scene} from 'phaser';
+import {createCenteredText} from "../helpers/text-helpers.ts";
 
 export class GameOver extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
     gameOverText: Phaser.GameObjects.Text;
+    mainMenuText: Phaser.GameObjects.Text;
 
     constructor() {
         super('GameOver');
@@ -13,11 +15,12 @@ export class GameOver extends Scene {
         this.camera = this.cameras.main
         this.camera.setBackgroundColor(0xff0000);
 
-        this.gameOverText = this.add.text(window.innerWidth / 2, window.innerHeight / 2, 'Game Over', {
-            fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
+        this.gameOverText = createCenteredText(this, 'Game Over', -100, 48, false);
+        this.mainMenuText = createCenteredText(this, 'Main Menu', 0, 32, true);
+
+        this.mainMenuText.on('pointerdown', () => {
+            this.scene.start('MainMenu')
+        });
 
         EventBus.emit('current-scene-ready', this);
     }
