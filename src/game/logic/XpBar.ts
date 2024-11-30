8557da2073fffa66a542fb8109225dfac53c3b05
Gegham Ayvazyan <gegham.aivazyan@gmail.com>
2024-12-01 02:21:54 +0400

    displaying level & current / next level xp

--- src/game/logic/XpBar.ts
+++ src/game/logic/XpBar.ts
@@ -1,10 +1,37 @@
-import Bar from "./Bar.ts";
-import Vector2Like = Phaser.Types.Math.Vector2Like;
+import Bar from './Bar.ts';
 import {COLOR_GREY, COLOR_WARNING} from '../helpers/colors.ts';
+import Vector2Like = Phaser.Types.Math.Vector2Like;
+import {formatNumber} from '../helpers/text-helpers.ts';
 
 class XpBar extends Bar {
-    constructor(scene: Phaser.Scene, position: Vector2Like, width: number, height: number, xpToNextLevel: number, currentXp: number = 0, positionOffset?: Vector2Like) {
+    level: number;
+    text: Phaser.GameObjects.Text;
+
+    constructor(scene: Phaser.Scene, position: Vector2Like, width: number, height: number, level: number, currentXp: number, xpToNextLevel: number, positionOffset?: Vector2Like) {
         super(scene, position, width, height, xpToNextLevel, currentXp, positionOffset, COLOR_WARNING, COLOR_GREY)
+        this.level = level;
+        console.log(this.bar.x, this.bar.y)
+        this.text = scene.add.text(position.x, position.y, '', {
+            fontFamily: 'Arial Black',
+            fontSize: 12,
+            color: '#ffffff',
+            stroke: '#000000',
+            strokeThickness: 4,
+            align: 'center',
+            // backgroundColor: '#000',
+        }).setFixedSize(width, height)
+    }
+
+    draw() {
+        super.draw();
+        this.writeLevelInfo();
+    }
+
+    writeLevelInfo() {
+        const from = formatNumber(this.currentValue);
+        const to = formatNumber(this.maxValue);
+        const levelInfo = `Level ${this.level} (${from}/${to})`
+        this.text?.setText(levelInfo);
     }
 }
 
