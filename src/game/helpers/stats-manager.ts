d0aea5cbf03e22946ff00bcac6293d5e04024639
Gegham Ayvazyan <gegham.aivazyan@gmail.com>
2024-12-01 18:22:57 +0400

    stats

--- src/game/helpers/stats-manager.ts
+++ src/game/helpers/stats-manager.ts
@@ -8,6 +8,22 @@ class StatsManager {
         this.agility = agility;
         this.endurance = endurance;
     }
+
+    listStats() {
+        // Finesse
+        //  - Dexterity / Agility
+        //  - affects Attack speed & Evade chance
+        // Awareness
+        //  - Perception & ???
+        //  - affects Critical chance / Critical damage & armor rating
+        // Resilience
+        //  - Strength & Endurance
+        //  - affects Attack damage & Max Health / Health Regen
+        // Thoughtfulness
+        //  - Intelligence / Charisma
+        //  - affects XP Gain & Bartering
+        return ['Finesse', 'Awareness', 'Resilience', 'Thoughtfulness'];
+    }
 }
 
 export default StatsManager;
