2f93dae3f5947c798b02bde7d0deaad72d249ef3
Gegham Ayvazyan <gegham.aivazyan@gmail.com>
2024-12-10 13:31:19 +0400

    changes

--- src/game/helpers/ui-helper.ts
+++ src/game/helpers/ui-helper.ts
@@ -1,30 +1,33 @@
 import {Scene} from "phaser";
 import {HEX_COLOR_GREY, HEX_COLOR_LIGHT_GREY, HEX_COLOR_WHITE} from "./colors.ts";
 import ScrollablePanel from "phaser3-rex-plugins/templates/ui/scrollablepanel/ScrollablePanel";
-import IConfig = ScrollablePanel.IConfig;
 import Sizer from "phaser3-rex-plugins/templates/ui/sizer/Sizer";
+import IConfig = ScrollablePanel.IConfig;
 
 class UiHelper {
-  static getDefaultScrollablePanelConfigs(scene: Scene, container: Sizer, x: number, y: number, width: number, height: number): IConfig {
+  static getDefaultScrollablePanelConfigs(scene: Scene, container: Sizer, x: number, y: number, width: number, height: number, configOverride: Partial<IConfig> = {}): IConfig {
     return {
-      x, y, width, height,
+      ...{
+        x, y, width, height,
 
-      scrollMode: 0, // 0 for vertical scrolling
-      background: scene.rexUI.add.roundRectangle(0, 0, 10, 10, 10, HEX_COLOR_LIGHT_GREY),
+        scrollMode: 0, // 0 for vertical scrolling
+        background: scene.rexUI.add.roundRectangle(0, 0, 10, 10, 10, HEX_COLOR_LIGHT_GREY),
 
-      panel: {
-        child: container,
+        panel: {
+          child: container,
 
-        mask: {
-          padding: 1
-        }
+          mask: {
+            padding: 1
+          }
+        },
+
+        slider: {
+          track: scene.rexUI.add.roundRectangle(0, 0, 20, 10, 10, HEX_COLOR_GREY),
+          thumb: scene.rexUI.add.roundRectangle(0, 0, 20, 30, 10, HEX_COLOR_WHITE)
+        },
+        space: {left: 10, right: 10, top: 10, bottom: 10, panel: 10}
       },
-
-      slider: {
-        track: scene.rexUI.add.roundRectangle(0, 0, 20, 10, 10, HEX_COLOR_GREY),
-        thumb: scene.rexUI.add.roundRectangle(0, 0, 20, 30, 10, HEX_COLOR_WHITE)
-      },
-      space: {left: 10, right: 10, top: 10, bottom: 10, panel: 10}
+      ...configOverride,
     };
   }
 }
