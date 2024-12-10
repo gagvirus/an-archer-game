e0e228c9cb8e4b7a4ca7e9dabfb5cdc500dee2b8
Gegham Ayvazyan <gegham.aivazyan@gmail.com>
2024-12-10 18:24:18 +0400

    refactoring resource list

--- src/game/modules/resource-list-module.ts
+++ src/game/modules/resource-list-module.ts
@@ -3,9 +3,15 @@ import {Scene} from "phaser";
 import Hero from "../logic/Hero.ts";
 import {AbstractModule} from "./module-manager.ts";
 import {ResourceType} from "../logic/drop/resource/Resource.ts";
+import ScrollablePanel from "phaser3-rex-plugins/templates/ui/scrollablepanel/ScrollablePanel";
+import Sizer from "phaser3-rex-plugins/templates/ui/sizer/Sizer";
+import UiHelper from "../helpers/ui-helper.ts";
+import {VectorZeroes} from "../helpers/position-helper.ts";
 
 class ResourceListModule extends AbstractModule {
-  private resourceTexts: Partial<{ [key in ResourceType]: Phaser.GameObjects.Text }> = {};
+  private container?: Sizer;
+  private panel?: ScrollablePanel;
+  private resources: Partial<{ [key: string]: Phaser.GameObjects.Text }> = {};
   private hero: Hero;
 
   constructor(scene: Scene, hero: Hero) {
@@ -14,28 +20,45 @@ class ResourceListModule extends AbstractModule {
   }
 
   start() {
-    let i = 0;
-    for (const [key, value] of Object.entries(ResourceType)) {
-      this.resourceTexts[value] = createText(this.scene, `${key}s: 0`, {
-        x: this.scene.scale.width - 75,
-        y: 80 + i * 25,
-      }, 16);
-      i++;
+    if (!this.container && !this.panel) {
+      this.container = this.scene.rexUI.add.sizer({orientation: "vertical", space: {item: 10}, width: 100, height: 100});
+
+      this.panel = this.scene.rexUI.add.scrollablePanel(UiHelper.getDefaultScrollablePanelConfigs(
+        this.scene, this.container, this.scene.scale.width - 80, 150, 150, 150,
+        {slider: false, align: {panel: 'right'}, /*background: undefined*/},
+      ))
+
+      Object.entries(ResourceType).forEach(([key, value]) => {
+        const resourceText = createText(this.scene, `BUBU${key}s: 0`, VectorZeroes(), 16, 'right');
+        this.resources[value] = resourceText;
+        this.container?.add(resourceText);
+      })
+
+      this.container.layout();
+      this.panel.layout();
     }
   }
 
   stop() {
-    for (const value of Object.values(ResourceType)) {
-      this.resourceTexts[value]?.destroy();
-      this.resourceTexts[value] = undefined;
+    if (this.container) {
+      this.container.destroy();
+      this.container = undefined;
     }
+    if (this.panel) {
+      this.panel.destroy();
+      this.panel = undefined;
+    }
+    Object.keys(ResourceType).forEach((key) => {
+      this.resources[key]?.destroy();
+      this.resources[key] = undefined;
+    })
   }
 
   update() {
     const resources = this.hero.getResources();
     Object.keys(resources).forEach((resourceType) => {
       const text = `${resourceType}s: ${formatNumber(resources[resourceType as ResourceType])}`;
-      this.resourceTexts[resourceType as ResourceType]?.setText(text);
+      this.resources[resourceType as ResourceType]?.setText(text);
     })
   }
 }
