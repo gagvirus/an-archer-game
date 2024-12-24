95c161fa3d6c5e05d35fb11d72cbc367a26ea788
Gegham Ayvazyan <gegham.aivazyan@gmail.com>
2024-12-24 23:15:59 +0400

    improvements on easy mode

--- src/game/stats/layers/difficulty-layer.ts
+++ src/game/stats/layers/difficulty-layer.ts
@@ -11,21 +11,14 @@ export class DifficultyLayer implements AttributeLayer {
   }
 
   modify(baseAttributes: Attributes): Attributes {
-    const difficultyMultiplier = isEasyMode(this.scene.game) ? 10 : 1;
     return {
       ...baseAttributes,
       [Attribute.xpRate]:
         baseAttributes.xpRate * (isRapidLevelUp(this.scene.game) ? 100 : 1),
-      [Attribute.dexterity]: baseAttributes.dexterity * difficultyMultiplier,
-      [Attribute.agility]: baseAttributes.agility * difficultyMultiplier,
-      [Attribute.perception]: baseAttributes.perception * difficultyMultiplier,
-      [Attribute.strength]: baseAttributes.strength * difficultyMultiplier,
-      [Attribute.fortitude]: baseAttributes.fortitude * difficultyMultiplier,
-      [Attribute.endurance]: baseAttributes.endurance * difficultyMultiplier,
-      [Attribute.intelligence]:
-        baseAttributes.intelligence * difficultyMultiplier,
-      [Attribute.charisma]: baseAttributes.charisma * difficultyMultiplier,
-      [Attribute.luck]: baseAttributes.luck * difficultyMultiplier,
+      [Attribute.damage]:
+        baseAttributes.damage * (isEasyMode(this.scene.game) ? 10 : 1),
+      [Attribute.health]:
+        baseAttributes.health * (isEasyMode(this.scene.game) ? 10 : 1),
     };
   }
 }
