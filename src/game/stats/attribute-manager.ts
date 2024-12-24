051861c57a57b8c397d1c14eed51c9c2fddc3a7f
Gegham Ayvazyan <gegham.aivazyan@gmail.com>
2024-12-24 16:31:31 +0400

    changes

--- src/game/stats/attribute-manager.ts
+++ src/game/stats/attribute-manager.ts
@@ -1,21 +1,59 @@
-import { Attributes } from "./attributes.ts";
+import { Attribute, Attributes } from "./attributes.ts";
 import { AttributeLayer } from "./attribute-layer.ts";
+import { HeroClassLayer } from "./layers/hero-class-layer.ts";
+import { HeroLevelLayer } from "./layers/hero-level-layer.ts";
+import { CoreStatsLayer } from "./layers/core-stats-layer.ts";
+import { StatsLayer } from "./layers/stats-layer.ts";
+
+enum LayerType {
+  heroClass = "HeroClass",
+  heroLevel = "HeroLevel",
+  coreStats = "CoreStats",
+  stats = "Stats",
+}
 
 export class AttributeManager {
-  private baseAttributes: Attributes;
-  private layers: AttributeLayer[];
+  private readonly baseAttributes: Attributes;
+  private layers: Record<LayerType, AttributeLayer>;
 
-  constructor(baseAttributes: Attributes) {
-    this.baseAttributes = { ...baseAttributes };
-    this.layers = [];
-  }
+  constructor() {
+    this.baseAttributes = {
+      [Attribute.agility]: 0,
+      [Attribute.armor]: 0,
+      [Attribute.armorRating]: 0,
+      [Attribute.attackRate]: 0,
+      [Attribute.barter]: 0,
+      [Attribute.baseAttackTime]: 0,
+      [Attribute.charisma]: 0,
+      [Attribute.criticalAmount]: 0,
+      [Attribute.criticalRate]: 0,
+      [Attribute.damage]: 0,
+      [Attribute.dexterity]: 0,
+      [Attribute.dropAmount]: 0,
+      [Attribute.dropRate]: 0,
+      [Attribute.endurance]: 0,
+      [Attribute.evadeRating]: 0,
+      [Attribute.fortitude]: 0,
+      [Attribute.health]: 0,
+      [Attribute.healthRegen]: 0,
+      [Attribute.healthRegenInterval]: 0,
+      [Attribute.intelligence]: 0,
+      [Attribute.luck]: 0,
+      [Attribute.perception]: 0,
+      [Attribute.strength]: 0,
+      [Attribute.xpRate]: 0,
+    };
 
-  addLayer(layer: AttributeLayer) {
-    this.layers.push(layer);
+    this.layers = {
+      [LayerType.heroClass]: new HeroClassLayer(),
+      [LayerType.heroLevel]: new HeroLevelLayer(),
+      [LayerType.coreStats]: new CoreStatsLayer(),
+      [LayerType.stats]: new StatsLayer(),
+    };
   }
 
   getFinalAttributes(): Attributes {
-    return this.layers.reduce(
+    return Object.values(this.layers).reduce(
       (currentAttributes, layer) => layer.modify(currentAttributes),
       { ...this.baseAttributes },
     );
--- src/game/stats/example.ts
+++ src/game/stats/example.ts
@@ -1,48 +1,9 @@
 // Initial base attributes
-import { Attribute, Attributes } from "./attributes.ts";
 import { AttributeManager } from "./attribute-manager.ts";
-import { HeroLevelLayer } from "./layers/hero-level-layer.ts";
-import { CoreStatsLayer } from "./layers/core-stats-layer.ts";
-import { HeroClassLayer } from "./layers/hero-class-layer.ts";
-import { StatsLayer } from "./layers/stats-layer.ts";
 
 export function getSample() {
-  const baseAttributes: Attributes = {
-    [Attribute.agility]: 0,
-    [Attribute.armor]: 0,
-    [Attribute.armorRating]: 0,
-    [Attribute.attackRate]: 0,
-    [Attribute.barter]: 0,
-    [Attribute.baseAttackTime]: 0,
-    [Attribute.charisma]: 0,
-    [Attribute.criticalAmount]: 0,
-    [Attribute.criticalRate]: 0,
-    [Attribute.damage]: 0,
-    [Attribute.dexterity]: 0,
-    [Attribute.dropAmount]: 0,
-    [Attribute.dropRate]: 0,
-    [Attribute.endurance]: 0,
-    [Attribute.evadeRating]: 0,
-    [Attribute.fortitude]: 0,
-    [Attribute.health]: 0,
-    [Attribute.healthRegen]: 0,
-    [Attribute.healthRegenInterval]: 0,
-    [Attribute.intelligence]: 0,
-    [Attribute.luck]: 0,
-    [Attribute.perception]: 0,
-    [Attribute.strength]: 0,
-    [Attribute.xpRate]: 0,
-  };
-
   // Create the manager
-  const attributeManager = new AttributeManager(baseAttributes);
-
-  // Add layers
-  attributeManager.addLayer(new HeroClassLayer()); // Default hero class
-  attributeManager.addLayer(new HeroLevelLayer()); // Hero is level 5
-  attributeManager.addLayer(new CoreStatsLayer());
-  attributeManager.addLayer(new StatsLayer());
-  // attributeManager.addLayer(new InventoryLayer({ health: 50, damage: 5 }));
+  const attributeManager = new AttributeManager();
 
   // Get final attributes
   const finalAttributes = attributeManager.getFinalAttributes();
