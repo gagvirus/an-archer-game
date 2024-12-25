a254505c20a1dd2f98d040a6904e4c52db27e21a
Gegham Ayvazyan <gegham.aivazyan@gmail.com>
2024-12-26 00:38:30 +0400

    displaying score on death screen

--- src/game/scenes/GameOver.ts
+++ src/game/scenes/GameOver.ts
@@ -3,11 +3,10 @@ import { Scene } from "phaser";
 import { createCenteredText } from "../helpers/text-helpers.ts";
 import { HEX_COLOR_DANGER } from "../helpers/colors.ts";
 import { ISceneLifecycle } from "../ISceneLifecycle.ts";
+import { StatisticsManager } from "../statistics/statistics-manager.ts";
 
 export class GameOver extends Scene implements ISceneLifecycle {
   camera: Phaser.Cameras.Scene2D.Camera;
-  gameOverText: Phaser.GameObjects.Text;
-  mainMenuText: Phaser.GameObjects.Text;
 
   constructor() {
     super("GameOver");
@@ -17,12 +16,13 @@ export class GameOver extends Scene implements ISceneLifecycle {
     this.camera = this.cameras.main;
     this.camera.setBackgroundColor(HEX_COLOR_DANGER);
 
-    this.gameOverText = createCenteredText(this, "Game Over", -100, 48, false);
-    this.mainMenuText = createCenteredText(this, "Main Menu", 0, 32, true);
-
-    this.mainMenuText.on("pointerdown", () => {
+    createCenteredText(this, "Game Over", -100, 48, false);
+    createCenteredText(this, "Main Menu", 0, 32, true).on("pointerdown", () => {
       this.scene.start("MainMenu");
     });
+
+    const score = StatisticsManager.getInstance().getScore();
+    createCenteredText(this, `Score: ${score}`, 100, 28);
 
     EventBus.emit("current-scene-ready", this);
   }
