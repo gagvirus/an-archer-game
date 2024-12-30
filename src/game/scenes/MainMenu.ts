563609ae00daad574ada8043f9e2e73da96b697c
Gegham Ayvazyan <gegham.aivazyan@gmail.com>
2024-12-30 22:01:49 +0400

    add link to hero select

--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -10,21 +10,27 @@ export class MainMenu extends Scene implements ISceneLifecycle {
   }
 
   create() {
-    createCenteredText(this, "Main Menu", -100, 38, false);
-    createCenteredText(this, "Start Game", 0, 32, true).on(
+    createCenteredText(this, "Main Menu", -300, 38, false);
+    createCenteredText(this, "Start Game", -100, 32, true).on(
       "pointerdown",
       () => {
         this.scene.start("MainScene");
       },
     );
-    createCenteredText(this, "Settings", 100, 32, true).on(
+    createCenteredText(this, "Hero Select", 100, 32, true).on(
+      "pointerdown",
+      () => {
+        this.scene.start("HeroSelectScene");
+      },
+    );
+    createCenteredText(this, "Settings", 200, 32, true).on(
       "pointerdown",
       () => {
         this.scene.start("SettingsScene");
       },
     );
 
-    createCenteredText(this, "Statistics", 200, 32, true).on(
+    createCenteredText(this, "Statistics", 300, 32, true).on(
       "pointerdown",
       () => {
         this.scene.start("StatisticsScene");
