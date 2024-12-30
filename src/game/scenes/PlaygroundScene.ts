d1d9e954a809da6734aa4c9c85a6cd0bc512b9f3
Gegham Ayvazyan <gegham.aivazyan@gmail.com>
2024-12-31 03:10:32 +0400

    sample skill map

--- src/game/scenes/PlaygroundScene.ts
+++ src/game/scenes/PlaygroundScene.ts
@@ -12,6 +12,15 @@ class PlaygroundScene extends Scene implements ISceneLifecycle {
   create() {
     // Initialize the hero in the center of the canvas
     this.hero = new Hero(this, this.scale.width / 2, this.scale.height / 2);
+    this.input.keyboard?.on("keydown", (event: KeyboardEvent) => {
+      if (["1", "2"].includes(event.key)) {
+        const skillsMap = {
+          "1": "freeze",
+          "2": "barrage",
+        };
+        console.log(skillsMap[event.key as "1" | "2"]);
+      }
+    });
   }
 
   update(time: number, delta: number) {
