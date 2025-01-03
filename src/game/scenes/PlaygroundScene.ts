0cb1254d29cb2bc91d849bb58ed76ca24358b3f9
Gegham Ayvazyan <gegham.aivazyan@gmail.com>
2025-01-03 19:45:06 +0400

    wip on PlaygroundScene.ts

--- src/game/scenes/PlaygroundScene.ts
+++ src/game/scenes/PlaygroundScene.ts
@@ -1,8 +1,15 @@
 import AbstractGameplayScene from "./AbstractGameplayScene.ts";
+import Enemy from "../game-objects/Enemy.ts";
+import { EnemyDef } from "../game-objects/enemies.ts";
 
 class PlaygroundScene extends AbstractGameplayScene {
   constructor() {
     super("PlaygroundScene");
+  }
+
+  create() {
+    super.create();
+    this.createDummyEnemy();
   }
 
   protected registerEventListeners() {
@@ -16,6 +23,31 @@ class PlaygroundScene extends AbstractGameplayScene {
         console.log(skillsMap[event.key as "1" | "2"]);
       }
     });
+  }
+
+  private createDummyEnemy() {
+    const dummyDef: EnemyDef = {
+      attackDamage: 0,
+      attackRange: 0,
+      attacksPerSecond: 0,
+      drops: {},
+      maxHealth: Infinity,
+      maxLevel: 0,
+      minLevel: 0,
+      name: "Dummy",
+      scale: 1,
+      speed: 0,
+      type: "imp",
+      weight: 0,
+      xpAmount: 0,
+    };
+    const enemy = new Enemy(
+      this,
+      this.scale.width / 2 - 100,
+      this.scale.height / 2,
+      dummyDef,
+    );
+    this.enemies.add(enemy);
   }
 }
 
